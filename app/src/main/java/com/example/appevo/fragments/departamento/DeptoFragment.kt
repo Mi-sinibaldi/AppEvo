269ddf76656b2ca7e9cbfc810787ac1e48900c30
Michelle <mia_araujo92@hotmail.com>
2019-08-15 12:08:48 -0300

    Ajustes recycler e adapter

--- app/src/main/java/com/example/appevo/fragments/departamento/DeptoFragment.kt
+++ app/src/main/java/com/example/appevo/fragments/departamento/DeptoFragment.kt
@@ -2,14 +2,20 @@ package com.example.appevo.fragments.departamento
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import com.example.appevo.R
+import com.example.appevo.adapters.AdapterDepto
+import kotlinx.android.synthetic.main.fragment_depto.*
 
 class DeptoFragment : Fragment() {
 
+    lateinit var adapterDepto: AdapterDepto
+    lateinit var deptoList: List<AdapterDepto>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,10 +23,13 @@ class DeptoFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_depto, container, false)
 
-        val buttonAddDpto = view?.findViewById<Button>(R.id.buttonAddDpto)
-        buttonAddDpto?.setOnClickListener {
+        adapterDepto = AdapterDepto(deptoList, DeptoFragment.class.java)
+        recyclerViewDepto.adapter = AdapterDepto
+        recyclerViewDepto.layoutManager = LinearLayoutManager(this)
+        recyclerViewDepto.smoothScrollToPosition(deptoList.size)
 
-        }
+
+
 
     }
 
